e0c81137b5ddd93e38e24928c2da4b54c4e87ce6
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-23 16:10:29 +0000

    Completed assignment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {UsersService} from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
 
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
+  constructor(private usersService: UsersService) {};
 
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
-  }
+  activeUsers = this.usersService.activeUsers;
+  inactiveUsers = this.usersService.inactiveUsers;
 }
